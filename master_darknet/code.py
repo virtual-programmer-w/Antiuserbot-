c985273a2a7520ac72509e572c3470f55a5ca227
Virtual-programmer-n <122021970+Virtual-programmer-n@users.noreply.github.com>
2023-02-13 21:49:30 +0500

    Add files via upload

---
+++ master_darknet/code.py
@@ -0,0 +1,18 @@
+session = input ('press enter')
+
+useri = "virtual_programmer_n"
+
+first_name = "test"
+
+last_name = "familiya"
+
+about = "test"
+
+username = ""
+
+you_phone = "number"
+
+chat = "uz_telethon"
+#leavechannel, 
+
+msg_id = 33969
